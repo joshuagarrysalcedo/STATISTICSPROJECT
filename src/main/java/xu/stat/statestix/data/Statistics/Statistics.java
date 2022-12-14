f08cd8e27be176f3d65dea08911713d5fb3810d0
swa <joshuagarrysalcedo@gmail.com>
2022-12-14 13:03:04 +0800

    created statistics class

---
+++ src/main/java/xu/stat/statestix/data/Statistics/Statistics.java
@@ -0,0 +1,37 @@
+package xu.stat.statestix.data.Statistics;
+
+public abstract class Statistics {
+    private String subject;
+    private String description;
+
+    public Statistics(String subject, String description) {
+        this.subject = subject;
+        this.description = description;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Statistics{" +
+                "subject='" + subject + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+
+}
