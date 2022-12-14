17c6290a64524edb91a302de2a2ffe82646a5f26
swa <joshuagarrysalcedo@gmail.com>
2022-12-14 13:29:26 +0800

    updated mode

--- src/main/java/xu/stat/statestix/data/Statistics/Mode.java
+++ src/main/java/xu/stat/statestix/data/Statistics/Mode.java
@@ -1,4 +1,19 @@
 package xu.stat.statestix.data.Statistics;
 
+import org.apache.commons.math3.stat.StatUtils;
+import xu.stat.statestix.util.MathUTIL;
+
 public class Mode extends Statistics{
+    public double[] set;
+    public double mode;
+    public Mode() {
+        super();
+        setSubject("Mode");
+        setDescription("The mode is the value that appears most frequently in a data set");
+    }
+
+    public double findMode(double[] set){
+        StatUtils.mode(set);
+        return Double.parseDouble(MathUTIL.df.format(set));
+    }
 }
