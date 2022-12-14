4a0089b501024fe4b179795681bf3be84a3102bf
swa <joshuagarrysalcedo@gmail.com>
2022-12-14 12:39:43 +0800

    Added dependencies

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module xu.stat.finalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens xu.stat.statestix to javafx.fxml;
+    exports xu.stat.statestix;
+    exports xu.stat.statestix.controllers;
+    opens xu.stat.statestix.controllers to javafx.fxml;
+}
